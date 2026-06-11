892db4cc54f8c174cd5d9589f1188a7159fbab7d
Izaac Eduardo Soares <izaaceduardosoares24@gmail.com>
2026-06-10 21:39:55 -0300

    refactor: Altera nome de pasta e mescla arquivos de sucesso e falha em um só

--- tests/crudGame.spec.ts
+++ tests/crudGame.spec.ts
@@ -1,4 +1,26 @@
 import { test, expect } from "@playwright/test";
+//Criação de jogo com falha
+test("Criação de jogo com falha", async ({ page }) => {
+  await page.goto("http://localhost:8081/login");
+  await page.fill('input[name="email"]', "izaac@gmail.com");
+  await page.fill('input[name="password"]', "Izaac123!");
+  await page.getByRole("button", { name: "Entrar" }).click();
+  await expect(page).toHaveURL("http://localhost:8081/");
+  await page.goto("http://localhost:8081/admin/games/new");
+  await page.waitForSelector('form');
+  await page.fill('input[type="text"]', "salsicha");
+  await page.fill('input[type="date"]', "2022-02-25");
+  await page.locator('textarea').first().fill("cachorro salsicha");
+  await page.locator('textarea').nth(1).fill("cachorro salsicha feio e gordinho.");
+  await page.getByRole("button", { name: /Escolher categorias/ }).click();
+  await page.waitForTimeout(500);
+  await page.locator('button').filter({ hasText: /Terror|Crime/ }).first().click();
+  await page.locator('button[type="submit"]').first().click();
+  await expect(page.getByText("Envie uma capa ou informe uma URL de fallback.")).toBeVisible();
+  await page.waitForTimeout(500);
+  await page.getByRole("link", { name: "Cancelar" }).click();
+  await expect(page).toHaveURL("http://localhost:8081/admin/games");
+});
 
 //Criação de jogo com sucesso
 test("Criação de jogo com sucesso", async ({ page }) => {
@@ -28,6 +50,14 @@ test("Criação de jogo com sucesso", async ({ page }) => {
   await page.waitForTimeout(1500);
 
   await expect(page.getByRole("heading", { name: "salsicha" }).first()).toBeVisible();
+  //ediçaõ com falha
+  await page.getByRole("link", { name: "Editar" }).first().click();
+    await page.waitForSelector('form');
+    await page.fill('input[type="text"]', "salsicha editado");
+    await page.locator('textarea').first().fill("cachorro salsicha editado");
+    await page.locator('textarea').nth(1).fill("cachorro salsicha feio e gordinho editado.");
+     await page.locator('button[type="submit"]').first().click();
+
 
   //edição
     await page.getByRole("link", { name: "Editar" }).first().click();
