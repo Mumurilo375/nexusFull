dc612c250057a94468d180666f862610a6de05d4
Murilo Pequeno <murilinho.pequeno@gmail.com>
2026-08-19 19:46:21 -0300

    Feat: adiciona novo design para /comofunciona

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" />
         <Stack.Screen name="mobile" />
+        <Stack.Screen name="comofunciona" options={{ animation: "slide_from_right" }} />
         <Stack.Screen name="login" options={{ animation: "slide_from_right" }} />
         <Stack.Screen name="cadastro" options={{ animation: "slide_from_right" }} />
         <Stack.Screen name="checkout" options={{ animation: "slide_from_right" }} />
